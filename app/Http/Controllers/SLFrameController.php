1dcc68654c637e81df3542a0a8d604d7a1c31c98
mhdtaufikr <mtaufikramadhan272gmail.com>
2024-01-31 16:34:42 +0700

    done develop view and store checck sheet

--- app/Http/Controllers/SLFrameController.php
+++ app/Http/Controllers/SLFrameController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Commoninformation;
 use App\Models\Itemcheckgroup;
+use App\Models\Checksheet;
 
 class SLFrameController extends Controller
 {
@@ -34,14 +35,49 @@ class SLFrameController extends Controller
         $checkGroups = [1, 2, 3, 4, 5, 6];
         // Fetch all Itemcheckgroups for the specified CheckGroup values
         $itemCheckGroups = Itemcheckgroup::whereIn('CheckGroup', $checkGroups)->get()->groupBy('CheckGroup');
-        return view('slframe.index', compact('Commoninformation', 'itemCheckGroups'));
+        $checkSheet = Checksheet::where('CommonInfoID',$Commoninformation->CommonInfoID)->get();
+        return view('slframe.index', compact('Commoninformation', 'itemCheckGroups','noframe','checkSheet'));
     }
     public function submit(Request $request)
     {
-      dd($request->all());
-    
-        return redirect()->back()->with('success', 'Form submitted successfully!');
+        // Validate the request data if necessary
+        $request->validate([
+            'noframe' => 'required',
+            // Add validation rules for other fields if needed
+        ]);
+
+        // Find the Commoninformation record
+        $commonInformation = Commoninformation::where('NoFrame', $request->noframe)->first();
+
+        // Loop through the submitted data and store it in the checksheets table
+        foreach ($request->findingQC as $index => $findingQC) {
+            // Check if the data already exists in checksheets
+            $existingChecksheet = Checksheet::where([
+                'CommonInfoID' => $commonInformation->CommonInfoID,
+                'ItemCheck' => $findingQC,
+            ])->first();
+
+            // If the data doesn't exist, store it
+            if (!$existingChecksheet) {
+                $checksheet = new Checksheet([
+                    'CommonInfoID' => $commonInformation->CommonInfoID,
+                    'ItemCheck' => $findingQC,
+                    'checkGroup' => $request->checkGroup,
+                    'FindingQC' => '1',
+                    'RepairQC' => '1', // Assuming it's always 1 when storing FindingQC
+                    'RemarksQG' => $request->remarks,
+                    // Add other fields if needed
+                ]);
+
+                $checksheet->save();
+            }
+        }
+
+        // Redirect back to the show route
+        return redirect()->route('show', ['noframe' => $request->noframe])->with('success', 'Data has been submitted successfully!');
     }
+
+    
     
     
 }
--- resources/views/slframe/index.blade.php
+++ resources/views/slframe/index.blade.php
@@ -15,6 +15,11 @@
         width: 25px; /* Adjust the width as needed */
         height: 25px; /* Adjust the height as needed */
     }
+
+    .custom-checkbox {
+        width: 1.25em;  /* Adjust the width as needed */
+        height: 1.25em; /* Adjust the height as needed */
+    }
 </style>
 
 @section('content')
@@ -39,21 +44,66 @@
                         @else
                             <span class="text-danger">Unknown Status</span>
                         @endif
-                        <button class="btn btn-primary btn-md" data-bs-toggle="modal" data-bs-target="#additionalInfoModal">Submit</button>
+                        <button id="submitButton" class="btn btn-primary btn-md" data-bs-toggle="modal" data-bs-target="#additionalInfoModal" disabled>Submit</button>
                     </span>
                 </div>
-                
-                
             </div>
+            <script>
+                var checkboxStates = {};
             
+                $(document).ready(function () {
+                    $('.check-checkbox').on('change', function () {
+                        updateSubmitButtonState();
+                    });
+                });
             
+                function updateSubmitButtonState() {
+                    var allGroupsChecked = true;
+            
+                    $('.check-checkbox').each(function () {
+                        var group = $(this).data('group');
+                        checkboxStates[group] = $(this).prop('checked');
+                    });
+            
+                    for (var group in checkboxStates) {
+                        allGroupsChecked = allGroupsChecked && checkboxStates[group];
+                        console.log('Group: ' + group + ' - Checked: ' + checkboxStates[group]);
+                    }
+            
+                    console.log('All Groups Checked: ' + allGroupsChecked);
+            
+                    $('#submitButton').prop('disabled', !allGroupsChecked);
+                }
+            
+                // This function ensures that the modal is opened only if at least one checkbox is checked
+                $('#additionalInfoModal').on('show.bs.modal', function (event) {
+                    if (!Object.values(checkboxStates).some(state => state)) {
+                        // Prevent modal from opening if no checkboxes are checked
+                        event.preventDefault();
+                        alert('Please check at least one checkbox before submitting.');
+                    }
+                });
+            </script>
+                
             
             <div class="card-body text-center d-flex justify-content-center align-items-center">
                 <img src="{{ asset('assets/img/SL-Frame.PNG') }}" alt="" class="img-fluid">
             </div>
             <div class="row p-2">
                 @foreach ($itemCheckGroups as $checkGroup => $itemCheckGroup)
-                    <div class="col-md-2 col-sm-4 p-2">
+                    <div class="col-md-2 col-sm-4 p-2 mb-2">
+                        <div class="form-check text-center d-flex align-items-center justify-content-center mb-2">
+                            <div class="form-check text-center d-flex align-items-center justify-content-center mb-2">
+                                @php
+                                    $isCheckedGroup = $checkSheet->where('checkGroup', $checkGroup)->isNotEmpty();
+                                @endphp
+                                <input class="form-check-input check-checkbox custom-checkbox" type="checkbox" id="check{{ $checkGroup }}" {{ $isCheckedGroup ? 'checked' : '' }} onchange="updateSubmitButtonState('{{ $checkGroup }}')">
+                                <p class="mb-0 ml-2">Check</p>
+                            </div>
+                            
+                            
+                        </div>
+                        
                         <div class="card h-100" data-bs-toggle="modal" data-bs-target="#modal{{ $checkGroup }}">
                             <div class="card-header text-dark text-center">
                                 <strong>{{ $checkGroup }}</strong>
@@ -86,25 +136,47 @@
                                             </thead>
                                             <tbody>
                                                 @foreach ($itemCheckGroup as $item)
-                                                    <tr>
-                                                        <td>{{ $item->ItemCheck }}</td>
-                                                        <td>
-                                                            <div class="form-check d-flex justify-content-center">
-                                                                <input class="form-check-input bigger-checkbox" type="checkbox" name="findingQC[]" value="{{ $item->ItemCheck }}">
-                                                            </div>
-                                                        </td>
-                                                        <td>
-                                                            <div class="form-check d-flex justify-content-center">
-                                                                <input class="form-check-input bigger-checkbox" type="checkbox" name="repairQC[]" value="{{ $item->ItemCheck }}">
-                                                            </div>
-                                                        </td>
-                                                    </tr>
-                                                @endforeach
+                                                <tr>
+                                                    <td>{{ $item->ItemCheck }}</td>
+                                                    <td>
+                                                        <div class="form-check d-flex justify-content-center">
+                                                            @php
+                                                                $isCheckedFinding = $checkSheet->contains('ItemCheck', $item->ItemCheck);
+                                                                $remarksFinding = $isCheckedFinding ? $checkSheet->where('ItemCheck', $item->ItemCheck)->first()->RemarksQG : null;
+                                                            @endphp
+                                                            <input class="form-check-input bigger-checkbox finding-qc-checkbox" type="checkbox" name="findingQC[]" value="{{ $item->ItemCheck }}" {{ $isCheckedFinding ? 'checked' : '' }} onchange="handleFindingQCChange(this)">
+                                                        </div>
+                                                    </td>
+                                                    <td>
+                                                        <div class="form-check d-flex justify-content-center">
+                                                            @php
+                                                                $isCheckedRepair = $checkSheet->contains('ItemCheck', $item->ItemCheck);
+                                                                $remarksRepair = $isCheckedRepair ? $checkSheet->where('ItemCheck', $item->ItemCheck)->first()->RemarksQG : null;
+                                                            @endphp
+                                                            <input class="form-check-input bigger-checkbox repair-qc-checkbox" type="checkbox" name="repairQC[]" value="{{ $item->ItemCheck }}" {{ $isCheckedRepair ? 'checked' : 'disabled' }}>
+                                                        </div>
+                                                    </td>
+                                                </tr>
+                                            @endforeach
+                                            <script>
+                                            function handleFindingQCChange(findingQCCheckbox) {
+                                                // Get the corresponding RepairQC checkbox
+                                                var repairQCCheckbox = findingQCCheckbox.closest('tr').querySelector('.repair-qc-checkbox');
+                                            
+                                                // Enable/disable RepairQC based on the state of FindingQC
+                                                repairQCCheckbox.disabled = !findingQCCheckbox.checked;
+                                            
+                                                // If FindingQC is unchecked, also uncheck RepairQC
+                                                if (!findingQCCheckbox.checked) {
+                                                    repairQCCheckbox.checked = false;
+                                                }
+                                            }
+                                            </script>
                                             </tbody>
                                         </table>
                                         <hr>
                                         <label for="remarks" style="font-size: 1em;">Remarks</label>
-                                        <textarea class="form-control" name="remarks" id="remarks" rows="5"></textarea>
+                                        <textarea class="form-control" name="remarks" id="remarks" rows="5">{{ $remarksFinding ?: $remarksRepair ?: optional($checkSheet->where('checkGroup', $checkGroup)->first())->RemarksQG }}</textarea>
                                     </div>
                                     <div class="modal-footer">
                                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
@@ -116,64 +188,61 @@
                     </div>
                 @endforeach
             </div>
-<!-- ... Your existing code ... -->
+            <!-- ... Your existing code ... -->
 
-
-
-<!-- Modal for Additional Information -->
-<div class="modal fade" id="additionalInfoModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-md  modal-dialog-centered">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title">Additional Information</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <form action="{{ route('submit') }}" method="POST">
-                @csrf
-                <div class="modal-body">
-                    <div class="row">
-                        <div class="col-sm-6">
-                            <label for="noFrame" class="form-label">No Frame</label>
-                            <input  value="{{$Commoninformation->NoFrame}}" type="text" class="form-control" id="noFrame" name="noFrame">
+            <!-- Modal for Additional Information -->
+            <div class="modal fade" id="additionalInfoModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-md  modal-dialog-centered">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title">Additional Information</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
-                        <div class="col-sm-6">
-                            <label for="tglProd" class="form-label">Tanggal Production</label>
-                            <input  type="date" class="form-control" id="tglProd" name="tglProd" value="{{ now()->toDateString() }}">
-                        </div>                        
-                        <div class="col-sm-6">
-                            <label for="shift" class="form-label">Shift</label>
-                            <input type="number" class="form-control" id="shift" name="shift" value="{{ getShiftValue() }}" >
-                        </div>
-                        <div class="col-sm-6">
-                            <label for="nameOfQG" class="form-label">Name of QG</label>
-                            <input  value="{{ auth()->user()->name }}" type="text" class="form-control" id="nameOfQG" name="nameOfQG">
-                        </div>
+                        <form action="{{ route('submit') }}" method="POST">
+                            @csrf
+                            <div class="modal-body">
+                                <div class="row">
+                                    <div class="col-sm-6">
+                                        <label for="noFrame" class="form-label">No Frame</label>
+                                        <input  value="{{$Commoninformation->NoFrame}}" type="text" class="form-control" id="noFrame" name="noFrame">
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <label for="tglProd" class="form-label">Tanggal Production</label>
+                                        <input  type="date" class="form-control" id="tglProd" name="tglProd" value="{{ now()->toDateString() }}">
+                                    </div>                        
+                                    <div class="col-sm-6">
+                                        <label for="shift" class="form-label">Shift</label>
+                                        <input type="number" class="form-control" id="shift" name="shift" value="{{ getShiftValue() }}" >
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <label for="nameOfQG" class="form-label">Name of QG</label>
+                                        <input  value="{{ auth()->user()->name }}" type="text" class="form-control" id="nameOfQG" name="nameOfQG">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                <button type="submit" class="btn btn-primary">Submit</button>
+                            </div>
+                        </form>
                     </div>
                 </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="submit" class="btn btn-primary">Submit</button>
-                </div>
-            </form>
-        </div>
-    </div>
-</div>
-@php
-    function getShiftValue() {
-        $currentHour = date('H');
-
-        if ($currentHour >= 7 && $currentHour < 15) {
-            return 1;
-        } elseif ($currentHour >= 15 && $currentHour < 23) {
-            return 2;
-        } else {
-            // You may want to handle other cases or set a default value
-            return 1;
-        }
-    }
-@endphp
-<!-- ... Your existing code ... -->
-
+            </div>
+            @php
+                function getShiftValue() {
+                    $currentHour = date('H');
+        
+                    if ($currentHour >= 7 && $currentHour < 15) {
+                        return 1;
+                    } elseif ($currentHour >= 15 && $currentHour < 23) {
+                        return 2;
+                    } else {
+                        // You may want to handle other cases or set a default value
+                        return 1;
+                    }
+                }
+            @endphp
+            <!-- ... Your existing code ... -->
 
         </div>
     </div>
